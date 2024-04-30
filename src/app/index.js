81d1b996acfc0e3ca64d502295a232c1fe6b28cd
Guilherme-14 <132360130+Guilherme-14@users.noreply.github.com>
2024-04-30 15:47:41 -0300

    Componentizacao

---
+++ src/app/index.js
@@ -0,0 +1,33 @@
+import { View, Text, Image } from 'react-native';
+import { styles } from '../css/style';
+import Botao from '../components/botao';
+import InputTexto from '../components/inputTexto';
+import { useState } from 'react';
+
+export default function App() {
+  const [valida, setValida] = useState();
+  const atualizaTexto = (newText) =>{
+    console.log(newText);
+  }
+  return (
+    
+    <View style={styles.container}>
+      <Image 
+      style={{width:'80%', height:80, marginBottom:30}}
+      resizeMode="contain"
+      source={require('../../assets/facebook.png')}
+     />
+      <InputTexto label="Número de celularou email" dica="Número de celularou email"/>
+      <InputTexto valor={atualizaTexto} label="Senha" dica="Senha"/>
+
+      <Botao btn="Entrar" cor='#1877F2' src='home' />
+      <View style={styles.cont}>
+        <View style={styles.line}/>
+        <Text style={styles.text}>ou</Text>
+        <View style={styles.line}/>
+      </View>
+      <Botao btn="Criar nova conta" cor='' src='cadastrar' />
+    </View>
+  );
+}
+
